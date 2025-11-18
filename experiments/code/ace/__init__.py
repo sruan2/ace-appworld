8502bf39fa6d49a62b2a7e240f42fc770ad76d41
Jerry Ma <jerry.ma@sambanovasystems.com>
2025-11-18 14:19:18 -0800

    small fix

--- experiments/code/ace/__init__.py
+++ experiments/code/ace/__init__.py
@@ -2,12 +2,6 @@
 from appworld_experiments.code.ace.base_agent import BaseAgent
 from appworld_experiments.code.ace.evaluation_agent import Agent
 from appworld_experiments.code.ace.adaptation_agent import StarAgent
-from appworld_experiments.code.ace.full_code_reflexion import (
-    SimplifiedFullCodeReflexionAgent,
-)
-from appworld_experiments.code.ace.base_full_code_reflexion import (
-    BaseSimplifiedFullCodeReflexionAgent,
-)
 from appworld_experiments.code.ace.ipfuncall import SimplifiedIPFunCallAgent
 from appworld_experiments.code.ace.base_react import BaseSimplifiedReActAgent
 from appworld_experiments.code.ace.evaluation_react import SimplifiedReActAgent
